1b6f4d4c580fd094b8b874faced141c2529bc29d
Rex Tan <admin@hiya3d.com>
2020-12-17 17:09:51 +0800

    详情页重构

---
+++ src/views/goods/GoodsComment.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="goods-comment">
+        
+        comment
+    </div>
+</template>
+
+<script>
+import Vue from "vue";
+import { Lazyload } from "vant";
+
+Vue.use(Lazyload);
+
+export default {
+  name: "GoodsComment",
+  components: {
+
+  },
+  data() {
+    return {
+        active: 1,
+        current: 0
+    }
+  },
+  mounted() {
+      
+  },
+  methods: {
+    
+  },
+  watch: {
+
+  }
+};
+</script>
+
+<style lang="less" scoped>
+
+</style>
